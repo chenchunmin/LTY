5b593b5a191d4b291aa1eadb5405b62b88434d2c
chenchunmin <3236693575@qq.com>
2022-07-04 23:15:52 +0800

    feat:首页、转运须知、填写地址页面以及复制地址页面及功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,10 +2,44 @@
 const app = getApp()
 Page({
   data: {
-
+    show: false,
+    flag: 0,
   },
-
-  onLoad() {
-
+  immtransfer() {
+    this.setData({
+      show: true,
+    });
   },
+  onClose() {
+    this.setData({
+      show: false
+    });
+  },
+  changeflag(e) {
+    this.setData({
+      flag: e.target.dataset.index
+    })
+  },
+  // 转运须知
+  transfer() {
+    wx.navigateTo({
+      url: '/pages/notify/notify',
+    })
+  },
+  pagedown() {
+    this.setData({
+      show: false
+    });
+    wx.navigateTo({
+      url: '/pages/address/address',
+    })
+  },
+  Copyaddress() {
+    wx.navigateTo({
+      url: '/pages/copy/copy',
+    })
+  },
+  select(){
+    console.log(11);
+  }
 })
